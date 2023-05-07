978fa893370c9ae53c1a5e7e48c85e8f38c5f5d4
A_ito <BoxPistols@users.noreply.github.com>
2023-05-07 09:37:35 +0000

    Update KpiTable.js

--- src/KpiTable.js
+++ src/KpiTable.js
@@ -8,66 +8,38 @@ import {
   TableCell,
   TableBody,
 } from "@mui/material";
-import Data from "./kpi.json"; // 追加
+import Data from "./kpi.json";
 import { Box } from "@mui/system";
 
 export const KpiTable = () => {
   const [parents, setParents] = useState([]);
-  const [selectedParent, setSelectedParent] = useState(null);
+  const [selectedParent, setSelectedParent] = useState();
 
   useEffect(() => {
     setParents(Data);
   }, []);
 
-  // button
-  const handleParentClick = (parent) => {
-    setSelectedParent(parent);
-  };
-
-  // select
-  const handleParentSelect = (event) => {
-    setSelectedParent(
-      parents.find((parent) => parent.id === Number(event.target.value))
-    );
+  const handleParentSelect = () => {
+    const parent = parents.find((p) => p.id === Number(event.target.value));
+    setSelectedParent(parent ?? null);
   };
 
   return (
     <>
       <Box display="flex">
-        {/* button */}
-        <h3>Pick</h3>
-        {/* <div>
-          {parents.children.map((child) => (
-            <button key={child.id} onClick={() => handleParentClick(child)}>
-              {child.name}
-            </button>
-          ))}
-        </div> */}
         <div>
-          {selectedParent && (
-            <div key={selectedParent.id}>
-              <h1>{selectedParent.name}</h1>
-              {/* <p>{selectedParent.id}</p> */}
-              <ul>
-                {selectedParent.children.map((child) => (
-                  <li key={child.id}>
-                    {child.id} - {child.name}
-                  </li>
-                ))}
-              </ul>
+          <h3>ネストの子要素のchildrenのListを表示</h3>
+          {parents.map((parent) => (
+            <div key={parent.id}>
+              {parent.children?.map((child, index) => (
+                <p key={index}>{child.title}</p>
+              ))}
             </div>
-          )}
-          {/* FIXME: List */}
-          {/* <ul>
-            {parents.children.map((parents) => (
-              <li key={parents.id}>{parents.title}</li>
-            ))}
-          </ul> */}
+          ))}
         </div>
 
-        {/* Table */}
         <div>
-          <h3>Table Choose</h3>
+          <h3>選択されたParentに応じたテーブル</h3>
           <Select
             native
             value={selectedParent ? selectedParent.id : ""}
@@ -92,7 +64,7 @@ export const KpiTable = () => {
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {selectedParent.children.map((child) => (
+                  {selectedParent.children?.map((child) => (
                     <TableRow key={child.id}>
                       <TableCell>{child.id}</TableCell>
                       <TableCell>{child.name}</TableCell>
